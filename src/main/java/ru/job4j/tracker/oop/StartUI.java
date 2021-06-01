f81ca6e1e9f69f582886ee641ac0014fbe3d7359
macbook <79824177721@yandex.ru>
2021-06-01 14:37:25 +0500

    6. Date. Отображение даты. [#310017 #117335]

---
+++ src/main/java/ru/job4j/tracker/oop/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.oop;
+
+import ru.job4j.tracker.Item;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(formatter.format(item.getCreated()));
+
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/oop/UsagePackageDate.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker.oop;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+//6. Date. Отображение даты. [#310017 #117335]
+
+public class UsagePackageDate {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now(); //Дата сейчас
+        LocalTime currentTime = LocalTime.now(); // Время сейчас
+
+        // до форматирования
+        LocalDateTime currentDateTime = LocalDateTime.now(); // Дата и время сейчас
+
+        // после форматирования
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateTimeFormat = currentDateTime.format(formatter);
+        System.out.println("Текущая дата: " + currentDate);
+        System.out.println("Текущее время: " + currentTime);
+        System.out.println("Текущая дата и время до форматирования : " + currentDateTime);
+        System.out.println("Текущие время и дата после форматирования: " + currentDateTimeFormat);
+    }
+}
