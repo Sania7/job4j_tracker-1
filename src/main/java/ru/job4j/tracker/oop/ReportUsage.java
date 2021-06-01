ee9a4593ec9d35b9795c981aa6cb2f79c99c3190
macbook <79824177721@yandex.ru>
2021-06-01 08:58:32 +0500

    4. Переопределение [#174890 #117353]

---
+++ src/main/java/ru/job4j/tracker/oop/ReportUsage.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker.oop;
+
+
+//4. Переопределение [#174890 #117353]
+public class ReportUsage {
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("Report`s name", "Report`s body");
+        System.out.println(text);
+    }
+}
