12fcfb287b93efa5b06fd6f83acc9b6e2846aa05
macbook <79824177721@yandex.ru>
2021-06-01 09:02:18 +0500

    4. Переопределение [#174890 #117353]

--- src/main/java/ru/job4j/tracker/oop/JSONReport.java
+++ src/main/java/ru/job4j/tracker/oop/JSONReport.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker.oop;
 
+
+//5. Аннотация @Override [#174891 #117351]
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return "{ " + System.lineSeparator() + name + " name" + "," + System.lineSeparator() +
                 body + " body" + System.lineSeparator() + "}";
