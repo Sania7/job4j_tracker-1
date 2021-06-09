46daea65593a1ab97aaeed81cb20c07a44106cff
macbook <79824177721@yandex.ru>
2021-06-09 10:50:28 +0500

    3.2. Зачем нужно приведение типов [#310019 #117301]

--- src/main/java/job4j/test3/VehicleInTransit.java
+++ src/main/java/job4j/test3/VehicleInTransit.java
@@ -1,5 +1,6 @@
 package job4j.test3;
 
+// 3.2. Зачем нужно приведение типов [#310019 #117301]
 public class VehicleInTransit {
     public static void main(String[] args) {
         Vehicle train = new Train();
