532e3529eb255e149b279db4c697036418d021d1
macbook <79824177721@yandex.ru>
2021-06-16 21:22:49 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #117310]

---
+++ src/main/java/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package job4j.ex;
+
+
+//java.lang.IllegalArgumentException - метод вызывается с некорректными параметрами.
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(calc(4));
+    }
+    public static int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Index n is less than 0!");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
