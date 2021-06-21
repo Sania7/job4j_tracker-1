69b058ad4799ee9a8157660b28bff2d7cc4f3cd0
macbook <79824177721@yandex.ru>
2021-06-21 20:00:30 +0500

    4. final [#334073 #169036]

---
+++ src/main/java/job4j/final1/Airbus.java
@@ -0,0 +1,36 @@
+package job4j.final1;
+
+public class Airbus {
+    private static final int COUNT_ENGINE = 2;
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void printModel() {
+        System.out.println("Модель самолета " + name);
+    }
+    public void printCountEngine() {
+        int countEngine = "A380".equals(name) ? 4 : COUNT_ENGINE;
+        System.out.println(countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                +
+                "name='"
+                + name
+                + '\''
+                +
+                '}';
+    }
+}
---
+++ src/main/java/job4j/final1/Aircraft.java
@@ -0,0 +1,9 @@
+package job4j.final1;
+
+
+//4. final [#334073 #169036]
+public class Aircraft {
+    public final void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+}
